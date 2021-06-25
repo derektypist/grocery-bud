29ef6bf66d50e6b96bef3a3f357901511c96141e
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-25 13:36:32 +0000

    Added constants to select items in script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Select Items
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+// Edit Option
+
+// Event Listeners
+
+// Functions
+
+// Local Storage
+
+// Setup Items
